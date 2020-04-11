f1c226c7d7a9ee2a8b5e62786b09b7f740ef55e7
Darksice <Darksice99@gmail.com>
2020-04-11 17:27:43 +0200

    ajout du composite

--- src/main/java/fr/uvsq/pglp_5_1/PersonnelComposite.java
+++ src/main/java/fr/uvsq/pglp_5_1/PersonnelComposite.java
@@ -1,11 +1,38 @@
 package fr.uvsq.pglp_5_1;
 
+import java.util.ArrayList;
+
 public class PersonnelComposite implements PersonnelInterface{
 	
-	
-	
-	public void print() {
+	private ArrayList<PersonnelInterface> p;
+	private final int id;
+	private static int cpt = 0;
 		
+	public PersonnelComposite() {
+		id = cpt++;
+		p = new ArrayList<PersonnelInterface>();
+	}
+	
+	public PersonnelComposite add(final PersonnelInterface i) {
+		if(!p.contains(i))
+			p.add(i);
+		
+		return this;
+	}
+	
+	public PersonnelComposite remove(final PersonnelInterface i)
+	{
+		if(p.contains(i))
+			p.remove(i);
+		
+		return this;
+			
+	}
+	
+	public void print()
+	{
+		for(PersonnelInterface i : p)
+			i.print();
 	}
 
 }
