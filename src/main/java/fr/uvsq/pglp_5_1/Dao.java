9a58c03b94647de53a439b49f669892fed566cfb
Darksice <Darksice99@gmail.com>
2020-05-09 15:58:50 +0200

    daoPersonnelComposite

--- src/main/java/fr/uvsq/pglp_5_1/Dao.java
+++ src/main/java/fr/uvsq/pglp_5_1/Dao.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public interface Dao<T> {
 	
-	public abstract void serialize(T object, String file);
+	public void serialize(T object, String file);
 	
-	public abstract T deserialize(String file);
+	public T deserialize(String file);
 	
-	public abstract void add(T object);
+	public void add(T object);
 	
-	public abstract T get(int id);
+	public T get(int id);
 	
-	public abstract T update(T object);
+	public T update(T object);
 	
-	public abstract void remove(T object);
+	public void remove(T object);
 	
 }
---
+++ src/main/java/fr/uvsq/pglp_5_1/DaoPersonnelComposite.java
@@ -0,0 +1,37 @@
+package fr.uvsq.pglp_5_1;
+
+public class DaoPersonnelComposite implements Dao<PersonnelComposite> {
+	
+	
+	public void serialize(PersonnelComposite p, String file)
+	{
+		
+	}
+	
+	public PersonnelComposite deserialize(String file)
+	{
+		
+	
+	}
+	
+	public void add(PersonnelComposite p)
+	{
+		
+	}
+	
+	public PersonnelComposite get(int id)
+	{
+		
+	}
+	
+	public PersonnelComposite update(PersonnelComposite p)
+	{
+		
+	}
+	
+	public void remove(PersonnelComposite p)
+	{
+		
+	}
+	
+}
